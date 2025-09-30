39eae02e7eed2ab91d4d38927a7ed3c11eece905
Юлия (Джулия) <sixquestions666@gmail.com>
2025-09-30 15:44:58 +0300

    Update routes.py

---
+++ routes.py
@@ -0,0 +1,65 @@
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.models import User
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('profile'))
+        else:
+            flash('Неверный email или пароль.', 'danger')
+    return render_template('login.html', title='Вход', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', title='Профиль', user=current_user)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(original_username=current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        if form.new_password.data:
+            current_user.set_password(form.new_password.data)
+        db.session.commit()
+        flash('Ваш профиль был обновлён!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('edit_profile.html', title='Редактировать профиль', form=form)
